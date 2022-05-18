7f4244612c0ba7d221caa08a9cac7533c2d1e985
mikseros <mmadynski@gmail.com>
2022-05-18 21:27:59 +0200

    Dynamic UI logic.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { catchError, map, Observable, of, startWith } from 'rxjs';
 import { DataState } from './enum/data-state.enum';
+import { Status } from './enum/status.enum';
 import { AppState } from './interface/app-state';
 import { CustomResponse } from './interface/custom-response';
 import { ServerService } from './service/server.service';
@@ -13,6 +14,7 @@ import { ServerService } from './service/server.service';
 export class AppComponent implements OnInit{
   appState$: Observable<AppState<CustomResponse>>;
   readonly DataState = DataState;
+  readonly Status = Status;
 
 
   constructor(private serverService: ServerService) {}
